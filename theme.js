bd020a891f1bd7f6fb1c3de57f49b6e9a6436261
nkosikenny <117735539+Nkosikenny@users.noreply.github.com>
2024-06-11 13:46:31 +0000

    added light and dark mode css files

---
+++ theme.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const darkModeBtn=document.getElementById("dark-mode");
+    const lightModeBtn=document.getElementById("light-mode");
+    const themeStyleSheet=document.getElementById("theme-style");
+
+    
+})
